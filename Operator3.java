f4ea00d32210e94601d2aa453dafda0683e9f43e
esha995 <eshaaaagarwal996@gmail.com>
2023-09-24 21:41:57 +0530

    commit

---
+++ Operator3.java
@@ -0,0 +1,36 @@
+public class Operator3 {
+    public static void main(String args[])
+    {
+         int a=5, b= 8;
+         if(a>b)
+         {
+            System.out.println("a is greater");
+         }
+         else{
+            System.out.println("b is  greater");
+         }
+         if(a<b)
+         {
+            System.out.println("a is greater");
+         }
+         else{
+            System.out.println("b is  greater");
+         }
+         if(a>=b)
+         {
+            System.out.println("a is greater");
+         }
+         else{
+            System.out.println("b is  greater");
+         }
+         if(a<=b)
+         {
+            System.out.println("a is greater");
+         }
+         else{
+            System.out.println("b is  greater");
+         }
+    }
+    
+}
+//relation operator
---
+++ Operator4.java
@@ -0,0 +1,16 @@
+public class Operator4 {
+    public static void main(String args[]){
+        int a=10;
+        int b=15;
+        a=b;
+        System.out.println("a  and b value:" +a +b);
+        //------------------------------------------------------------
+        System.out.println(+(a|b));
+        System.out.println(+(a&b));
+        System.out.println(+(a^b));
+        System.out.println(+(~a));
+
+    }
+}
+//bitwise operator 
+// assignment operator
